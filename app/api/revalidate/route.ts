ba29fa745ba80741cb7fda9e92643c4957a2ec2c
Luke MacKenzie <mackenzieluke94@gmail.com>
2025-01-29 13:53:36 -0500

    Revalidate content page via vercel cron

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-// This endpoint is used by a Vercel cron job to revalidate the daily-review page
+// This endpoint is used by a Vercel cron job to revalidate the daily-review page and the content page
 // CRON_SECRET is set in the Vercel dashboard
 // The value of the variable will be automatically sent as an Authorization header when Vercel invokes your cron job.
 export async function GET(request: NextRequest) {
@@ -15,6 +15,8 @@ export async function GET(request: NextRequest) {
   try {
     // Revalidate the daily-review page
     revalidatePath('/daily-review');
+    // revalidate the content page
+    revalidatePath('/content');
     return NextResponse.json({ revalidated: true, now: Date.now() });
   } catch (err) {
     // If there was an error, Next.js will continue
