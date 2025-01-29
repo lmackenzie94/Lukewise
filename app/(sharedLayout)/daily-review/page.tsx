ddea0a45979792335601b1548853c6cee8eaea0d
Luke MacKenzie <mackenzieluke94@gmail.com>
2025-01-29 09:46:29 -0500

    change cron time

--- app/(sharedLayout)/daily-review/page.tsx
+++ app/(sharedLayout)/daily-review/page.tsx
@@ -7,7 +7,7 @@ import { Metadata } from 'next';
 import { CATEGORIES, SITE_DESCRIPTION, SITE_TITLE } from '@/app/constants';
 import { Suspense } from 'react';
 
-// This route is automatically revalidated at 5am each day via a Vercel cron job
+// This route is automatically revalidated at 7am each day via a Vercel cron job (NOTE: cron is in UTC time - 12pm UTC = 7am EST)
 // Additionally, using "searchParams" causes the page to be dynamic (i.e. server-rendered on demand) b/c it's value isn't known until request time
 
 const PAGE_TITLE = 'Daily Review';
