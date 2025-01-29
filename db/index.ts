8f3abe9dca1bd3323f582ffcee772e7be7f815ec
Luke MacKenzie <mackenzieluke94@gmail.com>
2025-01-29 12:41:33 -0500

    Add custom fetch configuration to Turso database client to cache requests

--- db/index.ts
+++ db/index.ts
@@ -9,7 +9,15 @@ config({ path: '.env.local' });
 
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+  fetch: (request: Request) => {
+    return fetch(request, {
+      cache: 'force-cache',
+      next: {
+        revalidate: false
+      }
+    });
+  }
 });
 
 export const db = drizzle(client, { schema });
